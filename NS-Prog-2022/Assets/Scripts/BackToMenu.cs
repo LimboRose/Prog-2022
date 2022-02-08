d44ef494ee8cd45c9dcc451e17e02e50e4d6b229
Jan Tomczuk <tomczukj@evehor.com>
2022-02-08 19:25:52 +0100

    WIP

---
+++ NS-Prog-2022/Assets/Scripts/BackToMenu.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BackToMenu : MonoBehaviour
+{
+
+    void Start()
+    {
+        
+        StartCoroutine(Restart());
+    }
+
+    IEnumerator Restart()
+    {
+        
+        Debug.Log("Started Coroutine" + Time.time);
+
+        
+        yield return new WaitForSeconds(2);
+
+
+        SceneManager.LoadScene(0);
+    }
+
+
+}
