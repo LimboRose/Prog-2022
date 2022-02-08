15cf7c2297e989356dbd3c8300f9a2effd7994f5
Jan Tomczuk <tomczukj@evehor.com>
2022-02-08 20:07:39 +0100

    Add more content

---
+++ NS-Prog-2022/Assets/DelayDestroy.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DelayDestroy : MonoBehaviour
+{
+    
+    void Start()
+    {
+        StartCoroutine(Destroy());
+    }
+
+    IEnumerator Destroy()
+    {
+
+        Debug.Log("Started Coroutine" + Time.time);
+
+
+        yield return new WaitForSeconds(5);
+
+        Destroy(gameObject);
+
+    }
+
+}
---
+++ NS-Prog-2022/Assets/collectible.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class collectible : MonoBehaviour
+{
+    
+    public GameHandler GH;
+    public AudioClip soulSound;
+
+    private void Start()
+    {
+        GH = GameObject.Find("Canvas").GetComponent<GameHandler>();
+        
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        GH.souls++;
+        AudioSource.PlayClipAtPoint(soulSound, transform.position,1f);
+        Destroy(gameObject);
+        
+    }
+}
